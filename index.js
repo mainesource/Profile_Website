cad4693cf8cc84f48274a3b147d94167b9eedad4
mainesource <corey.robinson@greenwaydevelopment.net>
2025-05-16 16:22:14 -0400

    cleanup added js

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Javascript is loaded');
+
+function changeTitleColor(color) {
+  const titleElement = document.getElementsByClassName("title")
+  for(let i = 0; i < titleElement.length; i++){
+    titleElement[i].computedStyleMap.color;
+  }    
+}
+
